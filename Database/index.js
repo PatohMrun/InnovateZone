48617c7392f0637bb0fed1dbf0dcf243c5e572b1
Young Professor <113968776+Young-Professor@users.noreply.github.com>
2023-03-24 21:55:34 +0300

    Added:Approval

--- Database/index.js
+++ Database/index.js
@@ -293,45 +293,42 @@ app.get("/Approval",(err, res)=>{
   //Selecting the pending Approval
   app.post("/Approved", async (req, res) => {
     const { email } = req.body; // get the email from the request body
-    const Approvals = "UPDATE Admins SET Approval = 'Approved' WHERE email = ?";
-    db.query(Approvals, [email], async (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Error updating Approval status");
-      } else {
-        console.log(`Updated Approval status for ${email}`);
-        let transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS,
-          },
-        });
   
-        try {
-          // send email using nodemailer
-          const emailRes = await transporter.sendMail({
-            from: { name: "Justus Gitau", address: process.env.EMAIL },
-            to: email,
-            replyTo: process.env.EMAIL,
-            subject: "Approval",
-            html: `
-              <p>Your account is Approved you can login now using this link</p>
-              <p>https://innovate-zone.vercel.app/login</p>
-              <h4>Message From: InnovateZone</h4>
-            `,
-          });
-          console.log("message sent");
-          res.status(200).json({
-            message: "Received the message and sent email!",
-          }); // added a response message
-        } catch (error) {
-          console.log(error);
-          res.status(500).send("Failed to send email");
-        }
-      }
-    });
+    try {
+      const Approvals = "UPDATE Admins SET Approval = 'Approved' WHERE email = ?";
+      await db.query(Approvals, [email]);
+      console.log(`Updated Approval status for ${email}`);
+  
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASS,
+        },
+      });
+  
+      // send email using nodemailer
+      const emailRes = await transporter.sendMail({
+        from: { name: "Justus Gitau", address: process.env.EMAIL },
+        to: email,
+        replyTo: process.env.EMAIL,
+        subject: "Approval",
+        html: `
+          <p>Your account is Approved you can login now using this link</p>
+          <p>https://innovate-zone.vercel.app/login</p>
+          <h4>Message From: InnovateZone</h4>
+        `,
+      });
+      console.log("message sent");
+      res.status(200).json({
+        message: "Received the message and sent email!",
+      }); // added a response message
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Failed to update approval status and send email");
+    }
   });
+  
   
   
 app.post("/signUps", async (req, res) => {
